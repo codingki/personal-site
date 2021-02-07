02177c67f11391590cf8d94ca6193c3221f7f38b
Nur Fikri <codingki@gmail.com>
2021-02-07 13:51:29 +0700

    remove unnecessary import on head

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -36,17 +36,7 @@ export default function SingleBlog({ data, content }) {
 					content="initial-scale=1.0, width=device-width"
 					key="viewport"
 				/>
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-					rel="stylesheet"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-					integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
-					crossorigin="anonymous"
-				/>
+
 				<link rel="icon" href="/favicon.png" />
 				<title>{blog.title}</title>
 				<meta name="description" content={blog.excerpt} />
--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,7 @@ export default function Home({ data }) {
 					content="initial-scale=1.0, width=device-width"
 					key="viewport"
 				/>
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-					rel="stylesheet"
-				/>
+
 				<link rel="icon" href="/favicon.png" />
 				<title>Nur Fikri | Front-end Developer</title>
 				<meta
