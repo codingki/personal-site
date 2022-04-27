7f2914f6d41ee50ad1e59cff46bee3c60a4b773f
Nur Fikri <codingki@gmail.com>
2022-04-27 21:01:44 +0700

    chore: update nextjs, update chromium

--- pages/api/social-image.ts
+++ pages/api/social-image.ts
@@ -1,39 +1,23 @@
 import chrome from "chrome-aws-lambda";
-// for Local
-// import puppeteer from "puppeteer";
 import absoluteUrl from "next-absolute-url";
-import querystring from "querystring";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   let browser = null;
   const { origin } = absoluteUrl(req);
-  const urlOrigin = `${origin}`;
 
   try {
-    // Prod
-    const title = req.query.title;
-    const description = req.query.description;
-    const path = req.query.path;
-    const query = querystring.stringify({ title, description, path });
-    const url = `${urlOrigin}/image?${query}`;
-    // Local
-    // const url = `https://${req.headers.host}/image`;
+    const title = req.query.title as string;
+    const description = req.query.description as string;
+    const path = req.query.path as string;
+    const params = new URLSearchParams({ title, description, path });
+    const url = `${origin}/image?${params}`;
 
     browser = await chrome.puppeteer.launch({
-      // Local
-      // args: [],
-      // defaultViewport: chrome.defaultViewport,
-      // executablePath: puppeteer.executablePath(),
-      // headless: chrome.headless,
-      // ignoreHTTPSErrors: true,
-
-      // Prod
-      args: chrome.args,
+      args: [],
       defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
       headless: chrome.headless,
-      ignoreHTTPSErrors: chrome.headless,
+      ignoreHTTPSErrors: true,
     });
     const page = await browser.newPage();
 
