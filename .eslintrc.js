1d398b9ee28c80fca3fd404755a2339edf70b302
Nur Fikri <codingki@gmail.com>
2023-09-30 02:02:07 +0700

    chore: graz style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 // @ts-check
 
-const { getTsconfigPath } = require("@strangelovelabs/style-guide/eslint/helpers");
+const { getTsconfigPath } = require("@graz-sh/style-guide/eslint/helpers");
 
 /** @type {import("eslint").Linter.Config} */
 const eslintConfig = {
   extends: [
-    require.resolve("@strangelovelabs/style-guide/eslint/browser-node"),
-    require.resolve("@strangelovelabs/style-guide/eslint/react"),
-    require.resolve("@strangelovelabs/style-guide/eslint/next"),
-    require.resolve("@strangelovelabs/style-guide/eslint/typescript"),
+    require.resolve("@graz-sh/style-guide/eslint/browser-node"),
+    require.resolve("@graz-sh/style-guide/eslint/react"),
+    require.resolve("@graz-sh/style-guide/eslint/next"),
+    require.resolve("@graz-sh/style-guide/eslint/typescript"),
   ],
   ignorePatterns: [".next", "node_modules", "out"],
   parserOptions: {
